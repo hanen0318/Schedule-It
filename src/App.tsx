4e7ec463cb41cdf8b5db4c0fc1b43f755b5039dc
hanen0318 <hanksyu0318@gmail.com>
2023-11-06 15:12:23 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { Inject, ScheduleComponent,Day, Week, WorkWeek, Month, EventSettingsModel } from '@syncfusion/ej2-react-schedule';
+import { registerLicense } from '@syncfusion/ej2-base';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1JpRGBGfV5yd0VHal1XTnZWUj0eQnxTdEZiWH9YcXdUQGBVVEV2XQ==')
+
+class App extends React.Component{
+
+  private localData:EventSettingsModel = {
+    dataSource: [{
+      EndTime: new Date(2023, 10, 6,4,0),
+      StartTime: new Date(2023, 10, 6, 5, 0),
+      Subject: 'School',
+      IsAllDay: true
+    }]
+  };
+  public render() {
+    return <ScheduleComponent 
+    currentView='Month'
+    eventSettings={this.localData}>
+      
+      <Inject services={[Day, Week, WorkWeek, Month]} />
+    </ScheduleComponent>
+  }
 }
 
 export default App;
